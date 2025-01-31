00bc21c71ab3765220f4c94ab8f1accec096af18
Anez12 <harsenaargretya@students.amikom.ac.id>
2025-01-31 21:59:20 +0700

    Remove unused import from LoginForm component

--- app/components/Login/LoginForm.jsx
+++ app/components/Login/LoginForm.jsx
@@ -2,7 +2,6 @@ import { useState } from 'react';
 import { signIn } from 'next-auth/react';
 import { useRouter } from 'next/navigation';
 import Link from 'next/link';
-import { set } from 'mongoose';
 
 export default function LoginForm() {
   const [email, setEmail] = useState('');
